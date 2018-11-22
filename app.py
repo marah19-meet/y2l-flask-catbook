5cbdecc3760c6237fdd795d0c1af1901e7b36c39
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 19:03:10 +0200

    l

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
-from database import get_all_cats,get_cat_by_id
+from flask import render_template, request
+from database import get_all_cats, get_cat_by_id, create_cat
 
 
 app = Flask(__name__)
@@ -14,7 +14,15 @@ def catbook_home():
 def view_details(id):
 	cats= get_cat_by_id(id)
 	return render_template("cat.html", cat=cats)
-@app.route('/create_cat')
+@app.route('/create_cat',methods=['GET','POST'])
+def addpage():
+	if request.method=='GET':
+		return render_template("add.html")
+	else:
+		name= request.form['firstname']
+		create_cat(name)
+		cats= get_all_cats()
+		return render_template('home.html',cats=cats)
 
 if __name__ == '__main__':
 	app.run(debug = True)
